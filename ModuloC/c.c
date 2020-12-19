a5ef19ccc72de47ca9ca97b13f4a439feefe58e3
augusto021 <a93326@alunos.uminho.pt>
2020-12-19 18:27:08 +0000

    Nova função módulo c

--- ModuloC/c.c
+++ ModuloC/c.c
@@ -6,11 +6,38 @@ SINAL moduloC(char* file_name)
 
 }
 
-SINAL matrix_code(FICHEIRO fp){
+SINAL matrix_code(char *nome){
+
+    int qtlidas, n_blocos, size, x = 0, y = 0;
     
+    char c, modo;
+    char *binstr;
+    FILE *fp = fopen(nome,"r");
+    qtlidas = fscanf(fp,"@%c@%lld@%lu@",&modo,&n_blocos,&size);
+    unsigned long arrblocos[n_blocos];
+    arrblocos[0] = size;
 
+    char *matrix[n_blocos][256];
 
-
-
+    char *temp = binstr;
+    int index = 1;
+    for(int i = n_blocos;((c = fget(fp)) != EOF) && i > 0;i--){
+        if(c == ';'){
+            binstr = temp;
+            matrix[x][y] = binstr;
+            y++;
+        }
+        else if(c == '@'){
+            qtlidas = fscanf(fp,"@%lu@",&size);
+            arrblocos[index] = size;
+            index++;
+            y = 0;
+            x++;
+        }
+        else{
+            (*binstr) = c;
+            binstr++;
+        }
+    }    
 }
 
